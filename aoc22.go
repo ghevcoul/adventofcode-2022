e8a5104acd555a6103d0c0ba6f541cc970fd3f98
Gavin Heverly-Coulson <gavin.heverlycoulson@abcellera.com>
2022-12-01 11:35:08 -0800

    implement part B of the day 1 challenge

--- aoc22.go
+++ aoc22.go
@@ -4,33 +4,36 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
 func main() {
-	file, err := os.Open("day1a")
+	file, err := os.Open("inputs/day1")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	counts := make([]int, 0, 1000)
 	currentCount := 0
-	maxCount := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		if len(line) > 0 {
 			val, _ := strconv.Atoi(line)
 			currentCount += val
 		} else {
-			if currentCount > maxCount {
-				maxCount = currentCount
-			}
+			counts = append(counts, currentCount)
 			currentCount = 0
 		}
 	}
 
-	fmt.Println("The most calories carried by an elf is ", maxCount)
+	sort.Sort(sort.Reverse(sort.IntSlice(counts)))
+
+	fmt.Println("The most calories carried by an elf is ", counts[0])
+	topThree := counts[0] + counts[1] + counts[2]
+	fmt.Println("The total calories carried by the top three elves is", topThree)
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
