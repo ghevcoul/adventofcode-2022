7a55eb2d3f0e7853736bdf37e76be2c06f32f450
Gavin Heverly-Coulson <gavin.heverlycoulson@abcellera.com>
2022-12-02 11:52:13 -0800

    refactor day 1 solution into a function, decompose reading input file into an iterator

--- aoc22.go
+++ aoc22.go
@@ -8,18 +8,41 @@ import (
 	"strconv"
 )
 
-func main() {
-	file, err := os.Open("inputs/day1")
+// An iterator that returns one line of a file at a time
+// Based on/stolen from https://bbengfort.github.io/2016/12/yielding-functions-for-iteration-golang/
+func Readlines(path string) (<-chan string, error) {
+	file, err := os.Open(path)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
-	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	chnl := make(chan string)
+	go func() {
+		for scanner.Scan() {
+			chnl <- scanner.Text()
+		}
+		close(chnl)
+	}()
+
+	return chnl, nil
+}
+
+func Day1() {
+	dataPath := "inputs/day01"
+	reader, err := Readlines(dataPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	counts := make([]int, 0, 1000)
 	currentCount := 0
-	for scanner.Scan() {
-		line := scanner.Text()
+	for line := range reader {
 		if len(line) > 0 {
 			val, _ := strconv.Atoi(line)
 			currentCount += val
@@ -34,8 +57,8 @@ func main() {
 	fmt.Println("The most calories carried by an elf is ", counts[0])
 	topThree := counts[0] + counts[1] + counts[2]
 	fmt.Println("The total calories carried by the top three elves is", topThree)
+}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Println(err)
-	}
+func main() {
+	Day1()
 }
