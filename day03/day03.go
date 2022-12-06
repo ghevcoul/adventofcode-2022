a67c62692584d9651e0df46352c9a1e1a54d8fab
Gavin Heverly-Coulson <gavin.heverlycoulson@abcellera.com>
2022-12-05 16:23:43 -0800

    implement day 3 part 2

--- day03/day03.go
+++ day03/day03.go
@@ -23,9 +23,27 @@ func SetIntersection(setA map[rune]struct{}, setB map[rune]struct{}) map[rune]st
 	return intersection
 }
 
-func Day3() {
-	fmt.Println("* * * * * * * * * * Day 03 * * * * * * * * * *")
-	dataPath := "inputs/day03"
+func PopulateSet(sack string) map[rune]struct{} {
+	set := make(map[rune]struct{})
+	for _, item := range sack {
+		set[item] = struct{}{}
+	}
+	return set
+}
+
+func ItemToPriority(item rune) int {
+	var priority int
+
+	if item > 97 {
+		priority = int(item) - 96
+	} else {
+		priority = int(item) - 38
+	}
+
+	return priority
+}
+
+func Day3a(dataPath string) {
 	reader, err := utils.Readlines(dataPath)
 	if err != nil {
 		fmt.Println(err)
@@ -37,25 +55,52 @@ func Day3() {
 		midpoint := len(line) / 2
 
 		// Populate sets of the items in each rucksack
-		sack1Set := make(map[rune]struct{})
-		for _, item := range line[:midpoint] {
-			sack1Set[item] = struct{}{}
-		}
-		sack2Set := make(map[rune]struct{})
-		for _, item := range line[midpoint:] {
-			sack2Set[item] = struct{}{}
-		}
+		sack1Set := PopulateSet(line[:midpoint])
+		sack2Set := PopulateSet(line[midpoint:])
 		// Find the common item between the sets
 		var commonItem rune
 		for item := range SetIntersection(sack1Set, sack2Set) {
 			commonItem = item
 		}
-		if commonItem > 97 {
-			prioritySum += int(commonItem) - 96
-		} else {
-			prioritySum += int(commonItem) - 38
-		}
+		prioritySum += ItemToPriority(commonItem)
 	}
 
 	fmt.Println("The total priority of the common items is ", prioritySum)
+}
+
+func Day3b(dataPath string) {
+	reader, err := utils.Readlines(dataPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	prioritySum := 0
+	currentGroups := make([]string, 0, 3)
+	for line := range reader {
+		currentGroups = append(currentGroups, line)
+		if len(currentGroups) == 3 {
+			elf1Set := PopulateSet(currentGroups[0])
+			elf2Set := PopulateSet(currentGroups[1])
+			elf3Set := PopulateSet(currentGroups[2])
+			commonItems := SetIntersection(SetIntersection(elf1Set, elf2Set), elf3Set)
+
+			var commonItem rune
+			for item := range commonItems {
+				commonItem = item
+			}
+			prioritySum += ItemToPriority(commonItem)
+			// Clear the current groups list
+			currentGroups = make([]string, 0, 3)
+		}
+	}
+
+	fmt.Println("The total priority of the elves' badges is ", prioritySum)
+}
+
+func Day3() {
+	fmt.Println("* * * * * * * * * * Day 03 * * * * * * * * * *")
+	dataPath := "inputs/day03"
+	Day3a(dataPath)
+	Day3b(dataPath)
 }
